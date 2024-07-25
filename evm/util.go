857b95089c39a25f5755175f3d87350bc3ba268d
liuwei <linvan6v@gmail.com>
2024-07-25 16:08:23 +0800

    chore: implement a debug function to print any obj as json

--- evm/util.go
+++ evm/util.go
@@ -1,6 +1,7 @@
 package evm
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
@@ -15,4 +16,13 @@ func ConvertHashToYuHash(hash common.Hash) (yu_common.Hash, error) {
 	} else {
 		return yu_common.Hash{}, errors.New(fmt.Sprintf("Expected hash to be 32 bytes long, but got %d bytes", len(hash.Bytes())))
 	}
+}
+
+func ObjToJson(obj interface{}) string {
+	byt, err := json.Marshal(obj)
+	if err != nil {
+		fmt.Printf("Error marshalling obj to json: %v\n", err)
+		return ""
+	}
+	return string(byt)
 }
