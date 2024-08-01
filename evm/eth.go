aeff4e63b4168e0d35c33a48e5105d7d90ad77cb
liuwei <linvan6v@gmail.com>
2024-08-01 15:28:32 +0800

    impl: upgrade default geth cfg, base on config.AllEthashProtocolChanges

--- evm/eth.go
+++ evm/eth.go
@@ -6,7 +6,6 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
-	"math"
 	"math/big"
 	"net/http"
 	"sync"
@@ -106,53 +105,30 @@ type GethConfig struct {
 
 // sets defaults on the config
 func SetDefaultGethConfig() *GethConfig {
-	cfg := defaultGethConfig()
-	cfg.ChainConfig.ChainID = big.NewInt(1)
-	if cfg.ChainConfig == nil {
-		cfg.ChainConfig = &params.ChainConfig{
-			ChainID:             big.NewInt(1),
-			HomesteadBlock:      new(big.Int),
-			DAOForkBlock:        new(big.Int),
-			DAOForkSupport:      false,
-			EIP150Block:         new(big.Int),
-			EIP155Block:         new(big.Int),
-			EIP158Block:         new(big.Int),
-			ByzantiumBlock:      new(big.Int),
-			ConstantinopleBlock: new(big.Int),
-			PetersburgBlock:     new(big.Int),
-			IstanbulBlock:       new(big.Int),
-			MuirGlacierBlock:    new(big.Int),
-			BerlinBlock:         new(big.Int),
-			LondonBlock:         new(big.Int),
-		}
+	cfg := &GethConfig{
+		ChainConfig: params.AllEthashProtocolChanges,
+		Difficulty:  big.NewInt(1),
+		Origin:      common.HexToAddress("0x0"),
+		Coinbase:    common.HexToAddress("0x0"),
+		BlockNumber: big.NewInt(0),
+		Time:        0,
+		GasLimit:    8000000,
+		GasPrice:    big.NewInt(1),
+		Value:       big.NewInt(0),
+		Debug:       false,
+		EVMConfig:   vm.Config{},
+		BaseFee:     big.NewInt(params.InitialBaseFee), // 1 gwei
+		BlobBaseFee: big.NewInt(params.BlobTxMinBlobGasprice),
+		BlobHashes:  []common.Hash{},
+		BlobFeeCap:  big.NewInt(0),
+		Random:      &common.Hash{},
+		State:       nil,
+		GetHashFn: func(n uint64) common.Hash {
+			return common.BytesToHash(crypto.Keccak256([]byte(new(big.Int).SetUint64(n).String())))
+		},
 	}
 
-	if cfg.Difficulty == nil {
-		cfg.Difficulty = new(big.Int)
-	}
-	if cfg.GasLimit == 0 {
-		cfg.GasLimit = math.MaxUint64
-	}
-	if cfg.GasPrice == nil {
-		cfg.GasPrice = new(big.Int)
-	}
-	if cfg.Value == nil {
-		cfg.Value = new(big.Int)
-	}
-	if cfg.BlockNumber == nil {
-		cfg.BlockNumber = new(big.Int)
-	}
-	if cfg.GetHashFn == nil {
-		cfg.GetHashFn = func(n uint64) common.Hash {
-			return common.BytesToHash(crypto.Keccak256([]byte(new(big.Int).SetUint64(n).String())))
-		}
-	}
-	if cfg.BaseFee == nil {
-		cfg.BaseFee = big.NewInt(params.InitialBaseFee)
-	}
-	if cfg.BlobBaseFee == nil {
-		cfg.BlobBaseFee = big.NewInt(params.BlobTxMinBlobGasprice)
-	}
+	cfg.ChainConfig.ChainID = big.NewInt(1)
 
 	return cfg
 }
@@ -164,30 +140,6 @@ func LoadEvmConfig(fpath string) *GethConfig {
 		logrus.Fatalf("load config file failed: %v", err)
 	}
 	return cfg
-}
-
-func defaultGethConfig() *GethConfig {
-	return &GethConfig{
-		ChainConfig: params.GoerliChainConfig,
-		Difficulty:  big.NewInt(1),
-		Origin:      common.HexToAddress("0x0"),
-		Coinbase:    common.HexToAddress("0x0"),
-		BlockNumber: big.NewInt(0),
-		Time:        0,
-		GasLimit:    8000000,
-		GasPrice:    big.NewInt(1),
-		Value:       big.NewInt(0),
-		Debug:       false,
-		EVMConfig:   vm.Config{},
-		BaseFee:     big.NewInt(1000000000), // 1 gwei
-		BlobBaseFee: big.NewInt(0),
-		BlobHashes:  []common.Hash{},
-		BlobFeeCap:  big.NewInt(0),
-		Random:      &common.Hash{},
-
-		State:     nil,
-		GetHashFn: nil,
-	}
 }
 
 func setDefaultEthStateConfig() *config.Config {
