7ce7a8f4979ef64afe87d800a867bf2f0569c0dc
liuwei <linvan6v@gmail.com>
2024-08-01 15:28:59 +0800

    fix: use signer by gethCfg (instead EIP155signer)

--- evm/ethrpc/api_backend.go
+++ evm/ethrpc/api_backend.go
@@ -325,7 +325,8 @@ func (e *EthAPIBackend) SendTx(ctx context.Context, signedTx *types.Transaction)
 	}
 
 	// Create Tx
-	signer := types.NewEIP155Signer(e.ethChainCfg.ChainID)
+	head := e.CurrentBlock()
+	signer := types.MakeSigner(e.ChainConfig(), head.Number, head.Time)
 	sender, err := types.Sender(signer, signedTx)
 	if err != nil {
 		return err
