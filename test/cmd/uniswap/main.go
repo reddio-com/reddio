f90785aa1651ace1eb86cc5492479f61d3fd4065
haisenCx <sos963008@proton.me>
2024-09-09 14:02:36 +0800

    test: add uniswap Accuracy test

--- test/cmd/uniswap/main.go
+++ test/cmd/uniswap/main.go
@@ -69,7 +69,6 @@ func assertUniswapV2(ctx context.Context, evmCfg *evm.GethConfig) error {
 	ethManager.Configure(cfg, evmCfg)
 	ethManager.AddTestCase(
 		uniswap.NewUniswapV2AccuracyTestCase("UniswapV2 Accuracy TestCase", 2, cfg.InitialEthCount),
-		uniswap.NewUniswapV2TPSStatisticsTestCase("UniswapV2 TPS StatisticsTestCase", 2, cfg.InitialEthCount),
 	)
 	return ethManager.Run(ctx)
 }
