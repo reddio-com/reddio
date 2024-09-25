427321e349e262ef6ab4ff2848cd3e122e4d4027
ReyisaRuby <yisa@reddio.com>
2024-09-25 11:00:30 +0800

    revise stat
    Signed-off-by: ReyisaRuby <yisa@reddio.com>

--- test/transfer/benchmark_case.go
+++ test/transfer/benchmark_case.go
@@ -36,8 +36,11 @@ func (tc *RandomBenchmarkTest) Name() string {
 
 func (tc *RandomBenchmarkTest) Run(ctx context.Context, m *pkg.WalletManager) error {
 	transferCase := tc.tm.GenerateRandomTransferSteps(tc.steps, pkg.GenerateCaseWallets(tc.initialCount, tc.wallets))
-	if err := tc.rm.WaitN(ctx, tc.steps); err != nil {
-		return err
+	for _, step := range transferCase.Steps {
+		if err := tc.rm.Wait(ctx); err != nil {
+			return err
+		}
+		m.TransferEth(step.From, step.To, step.Count)
 	}
-	return m.BatchTransferETH(transferCase.Steps)
+	return nil
 }
