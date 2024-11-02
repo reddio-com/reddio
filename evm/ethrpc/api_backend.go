be04135a0e3d665c86a18aaffab07b21b777f96a
Lawliet-Chan <1576710154@qq.com>
2024-11-02 19:10:57 +0800

    add log

--- evm/ethrpc/api_backend.go
+++ evm/ethrpc/api_backend.go
@@ -441,12 +441,14 @@ func (e *EthAPIBackend) GetTransaction(ctx context.Context, txHash common.Hash) 
 		ethRcpt := new(types.Receipt)
 		err = json.Unmarshal(receipt.Extra, ethRcpt)
 		if err != nil {
-			logrus.Error("GetTransaction() json.Unmarshal failed: ", err)
+			logrus.Error("GetTransaction() json.Unmarshal eth receipt failed: ", err)
 			return true, ethTxn, common.Hash(blockHash), uint64(blockNumber), 0, err
 		}
 		index = uint64(ethRcpt.TransactionIndex)
 	}
 
+	logrus.Printf("EthAPIBackend.GetTransaction() blockHash %s, blockNumber %d, txIdx %d", blockHash.String(), blockNumber, index)
+	
 	return true, ethTxn, common.Hash(blockHash), uint64(blockNumber), index, nil
 }
 
