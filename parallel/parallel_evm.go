a7f45c7a5c01c04a5c2c8ea9f0bcb5207025b22f
ReyisaRuby <yisa@reddio.com>
2024-11-25 11:59:58 +0800

    fix (#167)
    Signed-off-by: ReyisaRuby <yisa@reddio.com>

--- parallel/parallel_evm.go
+++ parallel/parallel_evm.go
@@ -36,11 +36,58 @@ type ParallelEVM struct {
 	*tripod.Tripod
 	Solidity    *evm.Solidity `tripod:"solidity"`
 	statManager *BlockTxnStatManager
+	objectInc   map[common2.Address]int
 }
 
 func NewParallelEVM() *ParallelEVM {
 	return &ParallelEVM{
 		Tripod: tripod.NewTripod(),
+	}
+}
+
+func (k *ParallelEVM) prepareExecute() {
+	k.Solidity.StateDB().ClearPendingCommitMark()
+	k.clearObjInc()
+}
+
+func (k *ParallelEVM) clearObjInc() {
+	k.objectInc = make(map[common2.Address]int)
+}
+
+func (k *ParallelEVM) updateTxnObjSub(txns []*txnCtx) {
+	sub := func(key common2.Address) {
+		v, ok := k.objectInc[key]
+		if ok {
+			if v == 1 {
+				delete(k.objectInc, key)
+				return
+			}
+			k.objectInc[key] = v - 1
+		}
+	}
+	for _, txn := range txns {
+		addr1 := txn.req.Address
+		if addr1 != nil {
+			sub(*addr1)
+		}
+		sub(txn.req.Origin)
+	}
+}
+func (k *ParallelEVM) updateTxnObjInc(txns []*txnCtx) {
+	inc := func(key common2.Address) {
+		v, ok := k.objectInc[key]
+		if ok {
+			k.objectInc[key] = v + 1
+			return
+		}
+		k.objectInc[key] = 1
+	}
+	for _, txn := range txns {
+		addr1 := txn.req.Address
+		if addr1 != nil {
+			inc(*addr1)
+		}
+		inc(txn.req.Origin)
 	}
 }
 
@@ -51,7 +98,9 @@ func (k *ParallelEVM) Execute(block *types.Block) error {
 		k.statManager.ExecuteDuration = time.Since(start)
 		k.statManager.UpdateMetrics()
 	}()
+	k.prepareExecute()
 	txnCtxList, receipts := k.prepareTxnList(block)
+	k.updateTxnObjInc(txnCtxList)
 	got := k.splitTxnCtxList(txnCtxList)
 	got = k.executeAllTxn(got)
 	for _, subList := range got {
@@ -177,6 +226,10 @@ func (k *ParallelEVM) executeTxnCtxList(list []*txnCtx) []*txnCtx {
 		metrics.BatchTxnSplitCounter.WithLabelValues(strconv.FormatInt(int64(len(list)), 10)).Inc()
 		return k.executeTxnCtxListInConcurrency(k.Solidity.StateDB(), list)
 	}
+	defer func() {
+		k.updateTxnObjSub(list)
+		k.Solidity.StateDB().PendingCommit(true, k.objectInc)
+	}()
 	return k.executeTxnCtxListInOrder(k.Solidity.StateDB(), list, false)
 }
 
