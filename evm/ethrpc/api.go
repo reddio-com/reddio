983af4faedda076f525e84f08258eb85a7dbd7b2
Lawliet-Chan <1576710154@qq.com>
2024-11-27 16:35:09 +0800

    debug2

--- evm/ethrpc/api.go
+++ evm/ethrpc/api.go
@@ -71,6 +71,11 @@ func (s *EthereumAPI) GetLogs(ctx context.Context, crit FilterCriteria) ([]*type
 	if err != nil {
 		return nil, err
 	}
+
+	for _, flog := range logs {
+		logrus.Infof("EthereumAPI.GetLogs() blockHash(%s) log: %s", crit.BlockHash.String(), flog.TxHash.String())
+	}
+
 	if logs == nil {
 		return []*types.Log{}, nil
 	}
