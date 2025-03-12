c163083321940796c6b52704f6195a88844d2ff5
ReyisaRuby <yisa@reddio.com>
2025-03-12 12:00:59 +0800

    Fix
    Signed-off-by: ReyisaRuby <yisa@reddio.com>

--- parallel/evm_util.go
+++ parallel/evm_util.go
@@ -181,7 +181,6 @@ func (k *ParallelEVM) executeTxnCtxListInOrder(sdb *state.StateDB, list []*txnCt
 			tctx.receipt = k.handleTxnEvent(tctx.ctx, tctx.ctx.Block, tctx.txn, isRedo)
 		}
 		tctx.ps = tctx.ctx.ExtraInterface.(*pending_state.PendingStateWrapper)
-		sdb = tctx.ps.GetStateDB()
 		list[index] = tctx
 		if tctx.req.Address != nil && *tctx.req.Address == testBridgeContractAddress {
 			messageNonceSlot := sdb.GetState(testBridgeContractAddress, testStorageSlotHash)
@@ -193,11 +192,14 @@ func (k *ParallelEVM) executeTxnCtxListInOrder(sdb *state.StateDB, list []*txnCt
 				diff := new(big.Int).Sub(currentMessageNonceSlot, lastMessageNonceSlot)
 				if diff.Cmp(big.NewInt(1)) > 0 {
 					logrus.Warnf("Message nonce slot increased by more than 1: txhash %s, before %s, after %s, index %d ,diff %s,tctx.ctx.Block.Height %d", tctx.txn.TxnHash.String(), lastMessageNonceSlot.String(), currentMessageNonceSlot.String(), index, diff.String(), tctx.ctx.Block.Height)
-					metrics.WithdrawMessageNonceGap.WithLabelValues("bridge").Inc()
+					metrics.WithdrawMessageNonceGap.WithLabelValues("bridge", "more_increase").Inc()
+				}
+				if diff.Cmp(big.NewInt(0)) == 0 {
+					logrus.Warnf("Message nonce slot not changed: txhash %s, before %s, after %s, index %d ,diff %s,tctx.ctx.Block.Height %d", tctx.txn.TxnHash.String(), lastMessageNonceSlot.String(), currentMessageNonceSlot.String(), index, diff.String(), tctx.ctx.Block.Height)
+					metrics.WithdrawMessageNonceGap.WithLabelValues("bridge", "no_change").Inc()
 				}
 			}
 			lastMessageNonceSlot.Set(currentMessageNonceSlot)
-
 		}
 	}
 	k.gcCopiedStateDB(nil, list)
